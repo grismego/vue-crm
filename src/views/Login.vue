4bff2c334345ac67ab5ecc556c921b8de41ee6d6
Alexey Kunchiy <a.kunchiy@movavi.com>
2020-11-26 18:44:51 +0700

    validate login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -71,6 +71,7 @@
 
 <script>
 import { email, required, minLength } from "vuelidate/lib/validators";
+import messages from "../utils/messages";
 
 export default {
   name: "login",
@@ -81,6 +82,13 @@ export default {
   validations: {
     email: { email, required },
     password: { required, minLength: minLength(6) }
+  },
+  mounted() {
+    if (messages[this.$route.query.message]) {
+      this.$message(messages[this.$route.query.message]);
+    }
+    console.log(this);
+    this.$error("Test");
   },
   methods: {
     submitHandler() {
