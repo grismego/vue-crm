92a0bcecfacacd6d4d94da39ffbe1738509b51d6
Alexey Kunchiy <a.kunchiy@movavi.com>
2020-11-26 15:57:41 +0700

    navbar $ sidebar

---
+++ src/components/app/SideBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{ open: value }">
+    <router-link
+      v-for="link in links"
+      :exact="link.exact"
+      tag="li"
+      active-class="active"
+      :to="link.url"
+      :key="link.url"
+    >
+      <a href="#" class="waves-effect waves-orange pointer">{{ link.title }}</a>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+  data: () => ({
+    links: [
+      {
+        title: "Счет",
+        url: "/",
+        exact: true
+      },
+      {
+        title: "История",
+        url: "/history"
+      },
+      {
+        title: "Планирование",
+        url: "/planning"
+      },
+      {
+        title: "Новая запись",
+        url: "/record"
+      },
+      {
+        title: "Категории",
+        url: "/categories"
+      }
+    ]
+  })
+};
+</script>
