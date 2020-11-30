c8ba044d5b373774daa52b5d8f23dafd636e244c
Alexey Kunchiy <a.kunchiy@movavi.com>
2020-11-30 23:13:39 +0700

    + auth

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -89,12 +89,22 @@ export default {
     }
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch();
         return;
       }
-      this.$router.push("/");
+      const formData = {
+        email: this.email,
+        password: this.password
+      };
+
+      try {
+        await this.$store.dispatch("login", formData);
+        this.$router.push("/");
+      } catch (e) {
+        console.log(e);
+      }
     }
   }
 };
